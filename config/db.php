f9a5505d2af5f8b41bb6d68de28af62b59143ff4
Randel Palu <randel.palu@yandex.com>
2024-02-16 20:18:49 +0200

    Initial commit

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=mysql;dbname=yii_db',
+    'username' => 'yii_user',
+    'password' => 'user_password',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
